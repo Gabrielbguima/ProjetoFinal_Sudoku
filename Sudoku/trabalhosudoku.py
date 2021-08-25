f368c63ada1cce9434819e7a606c5e37051fd1bb
Gabrielbguima <86505410+Gabrielbguima@users.noreply.github.com>
2021-08-25 14:31:37 -0300

    Delete Sudoku directory

--- Sudoku/trabalhosudoku.py
+++
@@ -1,6 +0,0 @@
-#trabalho de comp2 Gabriel Batista Guimarães -- Sudoku
-class InterfaceUsuario():
-    def __init__(self, menuprincipal, telapause, telacarregar, telasalvar, opções):
-        pass
-
-class Mecanc
